9ea9865d145ba8262d05fd1195b66c9214ea991d
NaDiapla <nadiapla7@gmail.com>
2024-11-10 13:25:14 +0900

    chore: kakao login fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -32,13 +32,13 @@ const Login = () => {
     }
   }, []);
 
-  // useEffect(() => {
-  //   if (window.Kakao) {
-  //     if (!window.Kakao.isInitialized()) {
-  //       window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-  //     }
-  //   }
-  // }, [window.Kakao]);
+  useEffect(() => {
+    if (window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+      }
+    }
+  }, [window.Kakao]);
 
   const onKakaoLogin = () => {
     if (window.Kakao) {
