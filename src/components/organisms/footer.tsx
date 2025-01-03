bb1813d08c44deb2465bd5979676a166f75545a4
GoDoyeon <97godo@naver.com>
2025-01-03 23:11:23 +0900

    feat : footer 에도 반응형 추가

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -6,16 +6,18 @@ import FooterCopyright from "../molecules/footerCopyright";
 
 const Footer = () => {
   return (
-    <div className="mt-16 flex flex-col bg-background-base-light py-[80px] sm:w-full">
-      <section className="mx-[auto] w-[100%] max-w-[1000px]">
-        <FooterCompany />
-        <div className="mt-5">
-          <FooterLink />
-        </div>
-        <div className="mt-5">
-          <FooterCopyright />
-        </div>
-      </section>
+    <div className="mt-16 flex w-full flex-col items-center bg-background-base-light py-[80px]">
+      <div className="w-[90%] sm:w-[90%] md:w-[80%] lg:w-[70%]">
+        <section className="mx-[auto] w-[100%] max-w-[1000px]">
+          <FooterCompany />
+          <div className="mt-5">
+            <FooterLink />
+          </div>
+          <div className="mt-5">
+            <FooterCopyright />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
