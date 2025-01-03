7309b16f1770d266cfb1cae5eba4a3a1cb8385f7
dnwnvus <dnwnvus@gmail.com>
2025-01-04 03:39:47 +0900

    atoms checkbox 생성

--- src/components/atoms/chipItem.tsx
+++ src/components/atoms/chipItem.tsx
@@ -9,7 +9,7 @@ const ChipItem = ({ children, state, disabled, onClick }: ChipItemProps) => {
   return (
     <button
       type="button"
-      className={`hover:border-hover-chip active:border-pressed-chip flex h-auto w-auto items-center justify-center gap-1 rounded-lg border bg-background-surface-light px-3 py-[5px] ${state === "selected" ? "border-accent-primary-light text-accent-primary-light" : "border-border-default-light text-content-primary-light"} ${disabled && state === "selected" ? "border-accent-disabled-light text-accent-disabled-light" : "border-border-disabled-light text-content-disabled-light"}`}
+      className={`flex h-auto w-auto items-center justify-center gap-1 rounded-lg border bg-background-surface-light px-3 py-[5px] transition-colors duration-100 ease-linear hover:border-hover-chip active:border-pressed-chip ${state === "selected" ? "border-accent-primary-light text-accent-primary-light" : "border-border-default-light text-content-primary-light"} ${disabled && state === "selected" ? "border-accent-disabled-light text-accent-disabled-light" : "border-border-disabled-light text-content-disabled-light"}`}
       disabled={disabled}
       onClick={onClick}
     >
