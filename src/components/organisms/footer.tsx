e96960d37e3e20a76f39c1084c2522563ba6e764
GoDoyeon <97godo@naver.com>
2025-01-03 23:13:22 +0900

    feat : md 80 -> 85%로 수정

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -7,7 +7,7 @@ import FooterCopyright from "../molecules/footerCopyright";
 const Footer = () => {
   return (
     <div className="mt-16 flex w-full flex-col items-center bg-background-base-light py-[80px]">
-      <div className="w-[90%] sm:w-[90%] md:w-[80%] lg:w-[70%]">
+      <div className="w-[90%] sm:w-[90%] md:w-[85%] lg:w-[70%]">
         <section className="mx-[auto] w-[100%] max-w-[1000px]">
           <FooterCompany />
           <div className="mt-5">
