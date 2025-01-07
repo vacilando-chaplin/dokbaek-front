3ca5c8fee81a21854b9618da0f11170b5ae7fe40
dnwnvus <dnwnvus@gmail.com>
2025-01-08 02:02:20 +0900

    atom textArea, chips 생성

---
+++ src/components/atoms/chips.tsx
@@ -0,0 +1,52 @@
+import { sizeStyleType } from "@/types/types";
+import X from "../../../public/icons/X.svg";
+
+interface ChipsProps {
+  size: string;
+  text: string;
+  icon?: boolean;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const Chips = ({ size, text, icon, disabled, onClick }: ChipsProps) => {
+  const sizeStyle: sizeStyleType = {
+    large: "py-1.5 px-3 gap-1 text-body2 leading-body2 tracking-body2",
+    medium: "py-[5px] px-2 gap-0.5 text-body3 leading-body3 tracking-body3",
+    small: ""
+  };
+
+  const sizeStyleWithIcon: sizeStyleType = {
+    large: "py-1.5 pr-2 pl-3 gap-1 text-body2 leading-body2 tracking-body2",
+    medium:
+      "py-[5px] pr-1 pl-2 gap-0.5 text-body3 leading-body3 tracking-body3",
+    small: ""
+  };
+
+  return (
+    <div
+      className={`flex h-auto w-auto flex-row items-center rounded-lg bg-gray-100 transition-colors duration-100 ease-linear ${disabled && "border border-border-disabled-light"} ${icon ? sizeStyleWithIcon[size] : sizeStyle[size]}`}
+    >
+      <label
+        className={`${disabled ? "text-content-disabled-light" : "text-content-primary-light"}`}
+      >
+        {text}
+      </label>
+      {icon && (
+        <button
+          type="button"
+          className={`rounded-[4px] p-0.5 transition-colors duration-100 ease-linear hover:bg-[#f2f2f2] active:bg-[#E6E6E6] ${disabled && "bg-background-disabled-light"}`}
+          onClick={onClick}
+        >
+          <X
+            width="12"
+            height="12"
+            fill={`${disabled ? "#CED4DA" : "#868E96"}`}
+          />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Chips;
--- src/components/organisms/infoThird.tsx
+++ src/components/organisms/infoThird.tsx
@@ -1,5 +1,6 @@
 import { InfoInputType } from "@/types/types";
 import Title from "../atoms/title";
+import TextArea from "../atoms/textArea";
 
 interface InfoThirdProps {
   infoInputs: InfoInputType;
@@ -12,20 +13,14 @@ const InfoThird = ({ infoInputs, onInputChange }: InfoThirdProps) => {
   return (
     <section className="flex h-auto w-full flex-col gap-6 rounded-2xl bg-background-surface-light p-8">
       <Title name="자기 소개" />
-      <div className="flex h-40 w-full flex-col gap-1">
-        <textarea
-          placeholder="자기소개를 입력해주세요."
-          maxLength={500}
-          autoComplete="off"
-          name="introduction"
-          value={introduction}
-          onChange={onInputChange}
-          className="no-scrollbar h-full w-full resize-none items-start gap-1 rounded-xl border border-border-default-light bg-background-surface-light px-3 py-2 text-body3 font-normal leading-body3 tracking-body3 text-content-primary-light placeholder-content-tertiary-light outline-none"
-        />
-        <label className="flex h-fit w-full justify-end gap-1 text-caption1 font-normal leading-caption1 tracking-caption1 text-content-secondary-light">
-          {introduction.length}/500
-        </label>
-      </div>
+      <TextArea
+        size="medium"
+        name="introduction"
+        value={introduction}
+        limit={500}
+        placeholder="자기소개를 입력해주세요."
+        onChange={onInputChange}
+      />
     </section>
   );
 };
