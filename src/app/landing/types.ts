afc96a1b1ac248664859251503516f8a7a0348d0
GoDoyeon <97godo@naver.com>
2025-02-19 23:44:44 +0900

    feat: 프로필 좋아요, 좋아요 취소 api 연동

--- src/app/landing/types.ts
+++ src/app/landing/types.ts
@@ -1,11 +1,13 @@
 export interface ProfileShowcaseResponseType {
-  id?: number | null
-  status?: string | null
-  name?:string | null
-  bornYear?:number | null
-  height?:number | null
-  weight?:number | null
-  photoPreviewPaths?:string | null
-  createdAt?:string | null
-  updatedAt?:string | null
+  id?: number | null;
+  status?: string | null;
+  name?: string | null;
+  bornYear?: number | null;
+  height?: number | null;
+  weight?: number | null;
+  photoPreviewPaths?: string | null;
+  createdAt?: string | null;
+  updatedAt?: string | null;
+  likesCount?: number | null;
+  likedByMe?: boolean | null;
 }
--- src/app/profiles/api.ts
+++ src/app/profiles/api.ts
@@ -9,3 +9,22 @@ export const getProfiles = async (params: ProfileSearchParams) => {
     throw error;
   }
 };
+
+// 프로필 좋아요
+export const postProfileLike = async (profileId: number) => {
+  try {
+    const res = await api.post(`/profile/${profileId}/like`);
+    return res.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+// 프로필 좋아요 취소
+export const deleteProfileLike = async (profileId: number) => {
+  try {
+    const res = await api.delete(`/profile/${profileId}/like`);
+    return res.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
