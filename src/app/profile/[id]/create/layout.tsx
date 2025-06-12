566fee885767c9443c7ab5b6508e8b27cfade7db
dnwnvus <dnwnvus@gmail.com>
2025-06-12 17:49:27 +0900

    info 데이터중 null로 받아오는 string 초기값 빈 문자열로 설정

--- src/app/profile/[id]/create/layout.tsx
+++ src/app/profile/[id]/create/layout.tsx
@@ -3,6 +3,8 @@ import BottomBar from "@/app/profile/[id]/create/components/bottomBar";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import DraftModal from "./components/draftModal";
+import { getProfileDraftServer } from "@/lib/api/profile/common/api";
+import Initializer from "./components/Initializer";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const cookie = await cookies();
@@ -11,10 +13,24 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
 
   const profileId = Number(cookies().get("loginProfileId")?.value);
 
+  const profileInitData = await getProfileDraftServer(profileId);
+  const nullCheckedData = {
+    ...profileInitData,
+    info: {
+      ...profileInitData.info,
+      name: profileInitData.info.name ?? "",
+      email: profileInitData.info.email ?? "",
+      contact: profileInitData.info.contact ?? "",
+      instagramLink: profileInitData.info.instagramLink ?? "",
+      youtubeLink: profileInitData.info.youtubeLink ?? "",
+      introduction: profileInitData.info.introduction ?? ""
+    }
+  };
+
   return (
     <div className="relative mb-16 mt-16 flex flex-row justify-center gap-4 p-10">
       <ListMenu profileId={profileId} />
-      {children}
+      <Initializer profileInitData={nullCheckedData}>{children}</Initializer>
       <BottomBar profileId={profileId} />
       <DraftModal profileId={profileId} />
     </div>
