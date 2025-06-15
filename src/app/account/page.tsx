23a695a69867f969419d79b9d148e29a12a9e9e3
dnwnvus <dnwnvus@gmail.com>
2025-06-16 01:34:54 +0900

    마이 페이지에 이용약관, 개인정보 처리방침 링크 추가

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,14 +1,18 @@
-import AccountMainContents from "./components/accountMainContents";
-import AccountSubContents from "./components/accountSubContents";
+import { getTerms } from "@/lib/api";
+import AccountMenu from "./components/accountMenu";
+import AccountPolicies from "./components/accountPolicies";
 
-const Account = () => {
+const Account = async () => {
   // name 로그인 시 가져와야 함
+
+  const policies = await getTerms();
+
   return (
     <section className="flex h-auto min-w-[560px] max-w-[560px] flex-col items-center justify-center gap-10 pb-28 pt-24">
       <h1 className="typography-heading2 font-semibold">""님, 안녕하세요</h1>
       <div className="flex flex-col gap-2">
-        <AccountMainContents />
-        <AccountSubContents />
+        <AccountPolicies policies={policies.data.terms} />
+        <AccountMenu />
       </div>
     </section>
   );
