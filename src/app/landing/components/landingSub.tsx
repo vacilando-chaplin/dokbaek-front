b756d30634ce9e24321377a252df9dd4c80ed9cc
dnwnvus <dnwnvus@gmail.com>
2025-08-12 20:25:24 +0900

    홈페이지 배너 변경

--- src/app/landing/components/landingSub.tsx
+++ src/app/landing/components/landingSub.tsx
@@ -57,44 +57,33 @@ const LandingSub = () => {
   };
 
   return (
-    <div className="mx-[auto] mt-[40px] flex w-[100%] flex-col rounded-2xl">
+    <div className="relative mx-auto mt-10 flex h-60 w-full max-w-[1272px] flex-row items-center justify-between rounded-[40px] bg-[linear-gradient(to_bottom,#000000,#000003)] px-[100px]">
+      <img
+        src="/images/homeBannerImage.png"
+        alt="homeBannerImage"
+        className="absolute left-1/2 top-1/2 z-10 hidden h-[431px] w-[288px] -translate-x-1/2 -translate-y-1/2 object-contain shadow-[0_0_50px_0_rgba(0,0,0,0)] grayscale md:block"
+      />
       <div
-        className="relative h-[240px] px-[100px]"
+        className="absolute left-1/2 top-1/2 z-[8] hidden h-[240px] w-[300px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-opacity-50 bg-gradient-to-b from-static-white/30 to-static-white/0 md:block"
         style={{
-          background:
-            "linear-gradient(275.85deg, rgba(30, 133, 239, 0.08) 0%, rgba(99, 123, 255, 0.08) 100%)",
-          borderRadius: "40px"
+          filter: "blur(50px)"
         }}
+      />
+      <span className="text-[40px] font-bold leading-[100%] tracking-[-1%] text-static-white">
+        프로필은 독백에서
+      </span>
+      <BoxButton
+        type="secondaryOutlined"
+        size="large"
+        onClick={onMoveProfileEdit}
       >
-        <div className="">
-          <div className="absolute top-[80px]">
-            <p
-              style={{ lineHeight: "40px" }}
-              className="text-display font-bold text-accent-primary-light dark:text-accent-primary-dark"
-            >
-              프로필은
-            </p>
-          </div>
-          <div className="absolute left-[169px] top-[120px]">
-            <p
-              style={{ lineHeight: "40px" }}
-              className="text-display font-bold text-accent-primary-light dark:text-accent-primary-dark"
-            >
-              독백
-            </p>
-          </div>
-        </div>
-        <div className="absolute right-[100px] top-[95px]">
-          <BoxButton onClick={onMoveProfileEdit} type="primary" size="large">
-            프로필 만들기
-            <ArrowDirectionRight
-              width="16"
-              height="16"
-              className="fill-current text-content-on_color-light dark:text-content-on_color-dark"
-            />
-          </BoxButton>
-        </div>
-      </div>
+        프로필 만들기
+        <ArrowDirectionRight
+          width="16"
+          height="16"
+          className="fill-current text-content-primary-light dark:text-content-primary-dark"
+        />
+      </BoxButton>
       {loginModal && (
         <LoginModal onLoginModalClose={() => setLoginModal(false)} />
       )}
