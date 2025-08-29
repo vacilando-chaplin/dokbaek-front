90fd72b88f14c7a9232ac999f10808170b352a8f
dnwnvus <dnwnvus@gmail.com>
2025-08-29 19:30:08 +0900

    배우 프로필 openGraph 설정 추가

--- src/app/[@handle]/layout.tsx
+++ src/app/[@handle]/layout.tsx
@@ -6,6 +6,72 @@ import { getFilmoCategories, getProfileByHandleId } from "./api";
 import { profileInit } from "@/lib/data";
 import { Suspense } from "react";
 import TopNavigation from "@/components/organisms/topNavigation";
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
+  params
+}: {
+  params: { "@handle": string };
+}): Promise<Metadata> => {
+  const rawHandle = params["@handle"];
+  const handleName = decodeURIComponent(rawHandle).substring(1);
+
+  try {
+    const res = await getProfileByHandleId(handleName);
+
+    if (!res || !res.data) {
+      return {
+        title: "존재하지 않는 프로필 입니다.",
+        description: "존재하지 않는 배우의 프로필 입니다."
+      };
+    }
+
+    const profileData = res.data;
+    const profileImage =
+      profileData.mainPhotoPath || "/images/LogoHorizontalDokBaek.png";
+
+    return {
+      title: `${profileData.name}님의 프로필`,
+      description: `${profileData.name}님의 프로필을 확인하세요.`,
+      openGraph: {
+        title: `${profileData.name}님의 프로필`,
+        description: `${profileData.name}님의 프로필을 확인하세요.`,
+        url: `https://dokbaek.com/@${handleName}`,
+        siteName: "dokbaek",
+        images: [
+          {
+            url: profileImage,
+            width: 1200,
+            height: 630,
+            alt: `${profileData.name}님의 프로필 이미지`
+          }
+        ],
+        locale: "ko_KR",
+        type: "profile"
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: `${profileData.name} (@${handleName})`,
+        description: `${profileData.name}님의 프로필을 확인하세요.`,
+        images: [profileImage]
+      },
+      // 공개 프로필일 때의 웹 크롤링 설정
+      robots: {
+        index: true,
+        follow: true
+      }
+    };
+  } catch (error) {
+    return {
+      title: "배우님의 정보를 불러올 수 없는 프로필 입니다.",
+      description: "배우님의 프로필을 불러오는 도중 오류가 발생했어요.",
+      robots: {
+        index: false,
+        follow: false
+      }
+    };
+  }
+};
 
 const Layout = async ({
   params,
