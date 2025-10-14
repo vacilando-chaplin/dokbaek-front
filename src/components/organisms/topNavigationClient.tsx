55f0300314aa2af10b7729e5dfee1e4ecd6d9424
dnwnvus <dnwnvus@gmail.com>
2025-10-14 19:27:55 +0900

    탑 내비게이션 위치 조정 및 스타일 추가

--- src/components/organisms/topNavigationClient.tsx
+++ src/components/organisms/topNavigationClient.tsx
@@ -15,6 +15,7 @@ import { removeStorageData, setLoginProfileId } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import { routePaths } from "@/constants/routes";
 import { handleNameState } from "@/lib/recoil/handle/atom";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface UserMenuType {
   name: string;
@@ -107,67 +108,82 @@ const TopNavigationClient = () => {
   ];
 
   return (
-    <section className="fixed top-0 z-[48] flex h-12 w-full items-center border-b-[1px] border-border-default-light bg-background-elevated-light px-8 shadow-drop dark:border-border-default-dark dark:bg-background-elevated-dark">
-      <nav className="flex w-full items-center justify-between">
+    <section className="fixed top-0 z-[48] flex h-14 w-full items-center justify-center border-b-[1px] border-border-default-light bg-background-elevated-light shadow-drop dark:border-border-default-dark dark:bg-background-elevated-dark">
+      <nav className="container-max flex w-[90%] items-center justify-between sm:w-[90%] md:w-[85%] lg:w-[70%]">
         <Logo />
         {isLoggedIn === true && (
-          <div className="flex flex-row gap-5">
-            <Link
-              href={routePaths.profiles()}
-              className="typography-body3 flex items-center font-semibold text-content-secondary-light hover:text-accent-primary-light dark:text-content-secondary-dark dark:hover:text-accent-primary-dark"
-            >
-              배우 찾기
-            </Link>
-            <button
-              type="button"
-              className="typography-body3 flex items-center font-semibold text-content-secondary-light hover:text-accent-primary-light dark:text-content-secondary-dark dark:hover:text-accent-primary-dark"
-              onClick={onMoveMyProfile}
-            >
-              내 프로필
-            </button>
-            <Link href={routePaths.likes()} className="flex items-center">
-              <Heart
-                width="20"
-                height="20"
-                className="fill-current text-content-secondary-light dark:text-content-secondary-dark"
-              />
-            </Link>
-            <button
-              type="button"
-              className="relative flex h-9 w-9 items-center justify-center rounded-[100px] border border-border-default-light bg-gray-50 dark:border-border-default-dark dark:bg-gray-900"
-              onClick={onUserMenuClick}
-            >
-              <Person
-                width="20"
-                height="20"
-                className="fill-current text-content-secondary-light dark:text-content-secondary-dark"
-              />
-            </button>
-            {userMenuActive && (
-              <div className="scrollbar interaction-default absolute right-6 top-11 z-10 flex h-auto max-h-[332px] w-[120px] animate-enter list-none flex-col rounded-xl bg-background-elevated-light p-2 shadow-low dark:bg-background-elevated-dark">
-                {userMenu.map((item: UserMenuType) => {
-                  return (
-                    <button
-                      key={item.name}
-                      type="button"
-                      className="flex h-[38px] w-full cursor-pointer gap-2 rounded-md px-3 py-2 hover:bg-gray-50 active:bg-gray-150 dark:hover:bg-background-surface-dark dark:active:bg-gray-800"
-                      onClick={item.onClick}
-                    >
-                      <span className="typography-body3 font-regular text-content-primary-light dark:text-content-primary-dark">
-                        {item.name}
-                      </span>
-                    </button>
-                  );
-                })}
-              </div>
-            )}
+          <div className="flex flex-row items-center justify-center gap-3">
+            <div className="flex flex-row items-center justify-center gap-1">
+              <Link
+                href={routePaths.profiles()}
+                className="typography-body3 flex items-center rounded-lg px-2 py-1 font-semibold text-content-secondary-light hover:bg-hover-secondaryOutlined hover:text-accent-primary-light active:bg-pressed-secondaryOutlined dark:text-content-secondary-dark dark:hover:text-accent-primary-dark"
+              >
+                배우 찾기
+              </Link>
+              <button
+                type="button"
+                className="typography-body3 flex items-center rounded-lg px-2 py-1 font-semibold text-content-secondary-light hover:bg-hover-secondaryOutlined hover:text-accent-primary-light active:bg-pressed-secondaryOutlined dark:text-content-secondary-dark dark:hover:text-accent-primary-dark"
+                onClick={onMoveMyProfile}
+              >
+                내 프로필
+              </button>
+              <Link
+                href={routePaths.likes()}
+                className="group flex items-center gap-2.5 rounded-lg p-2 hover:bg-hover-secondaryOutlined active:bg-pressed-secondaryOutlined"
+              >
+                <Heart
+                  width="20"
+                  height="20"
+                  className="fill-current text-content-secondary-light group-hover:text-state-negative-light group-active:text-state-negative-light dark:text-content-secondary-dark"
+                />
+              </Link>
+            </div>
+            <div className="group relative">
+              <button
+                type="button"
+                className={`relative flex h-9 w-9 items-center justify-center rounded-[100px] border border-border-default-light bg-gray-50 hover:bg-hover-secondaryOutlined active:bg-pressed-secondaryOutlined dark:border-border-default-dark dark:bg-gray-900 ${userMenuActive && "bg-pressed-secondaryOutlined"}`}
+                onClick={onUserMenuClick}
+              >
+                <Person
+                  width="20"
+                  height="20"
+                  className="fill-current text-content-secondary-light dark:text-content-secondary-dark"
+                />
+              </button>
+              <AnimatePresence initial={false}>
+                {userMenuActive && (
+                  <motion.div
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: "auto", opacity: 1 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{ duration: 0.1, ease: "easeInOut" }}
+                    className="absolute right-0 top-10 z-10 flex h-auto max-h-[332px] w-[120px] list-none flex-col overflow-hidden rounded-xl bg-background-elevated-light p-2 shadow-low dark:bg-background-elevated-dark"
+                  >
+                    {userMenu.map((item: UserMenuType) => {
+                      return (
+                        <button
+                          key={item.name}
+                          type="button"
+                          className="flex h-[38px] w-full cursor-pointer gap-2 rounded-md px-3 py-2 hover:bg-gray-50 active:bg-gray-150 dark:hover:bg-background-surface-dark dark:active:bg-gray-800"
+                          onClick={item.onClick}
+                        >
+                          <span className="typography-body3 font-regular text-content-primary-light dark:text-content-primary-dark">
+                            {item.name}
+                          </span>
+                        </button>
+                      );
+                    })}
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
           </div>
         )}
         {isLoggedIn === false && (
           <div className="flex flex-row gap-5">
             <Link
               href={routePaths.profiles()}
-              className="typography-body3 flex items-center font-semibold text-content-secondary-light"
+              className="typography-body3 flex items-center rounded-lg px-2 py-1 font-semibold text-content-secondary-light hover:bg-hover-secondaryOutlined hover:text-accent-primary-light active:bg-pressed-secondaryOutlined dark:text-content-secondary-dark dark:hover:text-accent-primary-dark"
             >
               배우 찾기
             </Link>
