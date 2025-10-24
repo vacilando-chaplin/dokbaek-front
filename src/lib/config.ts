abd613ec2b009b414d09c994950b0932ccce11e8
dnwnvus <dnwnvus@gmail.com>
2025-10-24 22:58:01 +0900

    사이트 및 홈페이지 title, descriptions 변경

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
-  title: "독백 | 아티스트 섭외 플랫폼",
-  description: "아티스트 섭외 플랫폼",
+  title: "독백 | 배우 프로필, 배우 캐스팅 플랫폼",
+  description: "배우의 시작, 프로필 제작부터 캐스팅까지",
   siteUrl: process.env.NEXT_PUBLIC_BASEURL || "https://dokbaek.com",
   language: "ko"
 };
